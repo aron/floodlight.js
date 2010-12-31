ec7454fe4490dfeebf3d7af210673ed6ba0c7c6f
Aron Carroll <aron@3ev.com>
2010-12-31 17:12:38 +0000

    Implemented JavaScript syntax highlighting based on https://github.com/cloudhead/hijs

--- floodlight.js
+++ floodlight.js
@@ -1,11 +1,13 @@
-/* Floodlight (X)HTML Syntax Highlighter - v0.1
+/*  Floodlight (X)HTML Syntax Highlighter - v0.1
  *  Copyright 2010, Aron Carroll
  *  Released under the MIT license
  *  More Information: http://github.com/aron/floodlight.js
+ *  Based on hijs by Alexis Sellier https://github.com/cloudhead/hijs
  */
 
 (function (window, undefined) {
 	var _filters = {},
+	    _table = {},
 	    options  = {
 	    	prefix: '',
 	    	spaces: '  ',
@@ -26,6 +28,42 @@
 	function entities(string, type) {
 		return string.replace(options.regex[type], function (match) {
 			return options.map[type][match];
+		});
+	}
+
+	function encode(string) {
+		var characters = string.split(''),
+		    encoded = [],
+		    count = characters.length,
+		    index = 0;
+
+		for (; index < count;index += 1) {
+			if (characters[index].charCodeAt(0) > 127) {
+				encoded.push(characters[index]);
+			} else {
+				encoded.push(String.fromCharCode(characters[index].charCodeAt(0) + 0x2800));
+			}
+		}
+
+		encoded = encoded.join('');
+		_table[encoded] = string;
+		return encoded;
+	}
+
+	function decode(string) {
+		return _table[string];
+	}
+
+	function escapeMatch(string, klass) {
+		return '\u00ab' + encode(klass)  + '\u00b7'
+		                + encode(string) +
+		       '\u00b7' + encode(klass)  + '\u00bb';
+	}
+
+	function unescapeMatch(string) {
+		return string.replace(/\u00ab(.+?)\u00b7(.+?)\u00b7\1\u00bb/g, function (_, name, value) {
+			value = value.replace(/\u00ab[^\u00b7]+\u00b7/g, '').replace(/\u00b7[^\u00bb]+\u00bb/g, '');
+			return wrap(decode(value), decode(name));
 		});
 	}
 
@@ -50,30 +88,82 @@
 	function addFilter(name, regex, callback) {
 		_filters[name] = {regex: regex, callback: callback};
 	}
-	addFilter('trim', (/\t/g), function () { return options.spaces; });
+
+	// ! floodlight();
 
 	window.floodlight = function (source) {
 		return window.floodlight.html(source);
 	};
-
-	window.floodlight.html = (function () {
-		addFilter('html.tag', (/(<\/?)(\w+)([^>]*)(\/?>)/gi), function (match, open, tag, attr, close) {
-			var attributes = filter('html.attr', attr);
-			return wrap(open, 'bracket') + wrap(tag, 'tag') + attributes + wrap(close, 'bracket');
-		});
-		addFilter('html.attr', (/(\w+)(?:\s*=\s*("[^"]*"|'[^']*'|[^>\s]+))?/g), function (match, attr, value) {
-			return wrap(attr, 'attribute') + (value ? '=' + wrap(value, 'value') : '');
-		});
-		addFilter('html.comment', (/<!--[^\-]*-->/g), function (comment) {
-			return wrap(comment, 'comment');
-		});
-		
-		return function (source) {
-			return filter(['trim', 'html.tag', 'html.comment'], source);
-		};
-	}).call(floodlight);
-
+	window.floodlight.options = options;
 	window.floodlight.encode  = function (string) { return entities(string, 'encode'); };
 	window.floodlight.decode  = function (string) { return entities(string, 'decode'); };
-	window.floodlight.options = options;
+
+	addFilter('whitespace', (/\t/g), function () { return options.spaces; });
+
+	// ! floodlight.html();
+
+	window.floodlight.html = function (source) {
+		return filter(window.floodlight.html.filters, source);
+	};
+
+	(function () {
+		this.regex = {
+			tag:     /(<\/?)(\w+)([^>]*)(\/?>)/g,
+			attr:    /(\w+)(?:\s*=\s*("[^"]*"|'[^']*'|[^>\s]+))?/g,
+			comment: /<!--[^\-]*-->/g
+		};
+
+		this.filters = ['whitespace', 'html.tag', 'html.comment'];
+
+		addFilter('html.tag', this.regex.tag, function (match, open, tag, attr, close) {
+			var attributes = filter('html.attr', attr);
+			return wrap(open, 'html-bracket') + wrap(tag, 'html-tag') + attributes + wrap(close, 'html-bracket');
+		});
+
+		addFilter('html.attr', this.regex.attr, function (match, attr, value) {
+			return wrap(attr, 'html-attribute') + (value ? '=' + wrap(value, 'html-value') : '');
+		});
+
+		addFilter('html.comment', this.regex.comment, function (comment) {
+			return wrap(comment, 'html-comment');
+		});
+	}).call(window.floodlight.html);
+
+	// ! floodlight.javascript();
+
+	window.floodlight.javascript = function (source) {
+			return unescapeMatch(filter(window.floodlight.javascript.filters, source));
+	};
+
+	(function () {
+		var key;
+
+		this.keywords = ('var function if else for while break switch case do new null in with void '
+		                +'continue delete return this true false throw catch typeof with instanceof').split(' ');
+		this.special  = ('eval window document undefined NaN Infinity parseInt parseFloat '
+		                +'encodeURI decodeURI encodeURIComponent decodeURIComponent').split(' ');
+		this.regex = {
+			'comment': (/(\/\/[^\n]*|\/\*(?:[^*\n]|\*+[^\/*])*\*+\/)/g),
+		  'string':  (/("(?:(?!")[^\\\n]|\\.)*"|'(?:(?!')[^\\\n]|\\.)*')/g),
+		  'regexp':  (/(\/.+\/[mgi]*)(?!\s*\w)/g),
+		  'class':   (/\b([A-Z][a-zA-Z]+)\b/g),
+		  'number':  (/\b([0-9]+(?:\.[0-9]+)?)\b/g),
+		  'keyword': new(RegExp)('\\b(' + window.floodlight.javascript.keywords.join('|') + ')\\b', 'g'),
+		  'special': new(RegExp)('\\b(' + window.floodlight.javascript.special.join('|')  + ')\\b', 'g')
+		};
+
+		this.filters = ['whitepace'];
+
+		for (key in this.regex) {
+			(function (regex, name, filters) {
+				var namespace = 'javascript.' + name;
+
+				addFilter(namespace, regex, function (match, capture) {
+					return escapeMatch(capture, 'javascript-' + name);
+				});
+
+				filters.push(namespace);
+			})(window.floodlight.javascript.regex[key], key, this.filters);
+		}
+	}).call(window.floodlight.javascript);
 })(this);
--- tests/test.js
+++ tests/test.js
@@ -22,22 +22,65 @@ Riot.run(function() {
 
 			should('wrap comments', function () {
 				return floodlight('<!---->');
-			}).equals('<span class="comment">&lt;!----&gt;</span>');
+			}).equals('<span class="html-comment">&lt;!----&gt;</span>');
 
 			should('wrap elements in spans', function () {
 				return floodlight('<a href="#" title="Ooh a link">link</a>');
 			}).equals([
-				'<span class="bracket">&lt;</span>',
-				'<span class="tag">a</span> ',
-				'<span class="attribute">href</span>=',
-				'<span class="value">&quot;#&quot;</span> ',
-				'<span class="attribute">title</span>=',
-				'<span class="value">&quot;Ooh a link&quot;</span>',
-				'<span class="bracket">&gt;</span>link',
-				'<span class="bracket">&lt;/</span>',
-				'<span class="tag">a</span>',
-				'<span class="bracket">&gt;</span>'
+				'<span class="html-bracket">&lt;</span>',
+				'<span class="html-tag">a</span> ',
+				'<span class="html-attribute">href</span>=',
+				'<span class="html-value">&quot;#&quot;</span> ',
+				'<span class="html-attribute">title</span>=',
+				'<span class="html-value">&quot;Ooh a link&quot;</span>',
+				'<span class="html-bracket">&gt;</span>link',
+				'<span class="html-bracket">&lt;/</span>',
+				'<span class="html-tag">a</span>',
+				'<span class="html-bracket">&gt;</span>'
 			].join(''));
+		});
+	});
+
+	context('floodlight.javascript()', function () {
+		given('a simple JavaScript string', function () {
+			//should('replace tabs with spaces', floodlight('\t')).equals('  ');
+
+			should('wrap inline comments', function () {
+				return floodlight.javascript('// This is a comment');
+			}).equals('<span class="javascript-comment">// This is a comment</span>');
+
+			should('wrap multiline comments', function () {
+				return floodlight.javascript('/* This is a comment */');
+			}).equals('<span class="javascript-comment">/* This is a comment */</span>');
+
+			should('wrap strings and keywords', function () {
+				return floodlight.javascript('var test = "Hello World";');
+			}).equals([
+				'<span class="javascript-keyword">var</span> test = ',
+				'<span class="javascript-string">&quot;Hello World&quot;</span>;'
+			].join(''));
+
+			should('wrap integers', function () {
+				return floodlight.javascript('0; 1.23;');
+			}).equals([
+				'<span class="javascript-number">0</span>; ',
+				'<span class="javascript-number">1.23</span>;'
+			].join(''));
+
+			should('wrap functions', function () {
+				return floodlight.javascript('function test() {}');
+			}).equals('<span class="javascript-keyword">function</span> test() {}');
+
+			should('wrap classes', function () {
+				return floodlight.javascript('function Test() {}');
+			}).equals([
+				'<span class="javascript-keyword">function</span> ',
+				'<span class="javascript-class">Test</span>() {}'
+			].join(''));
+
+			should('wrap regular expressions', function () {
+				return floodlight.javascript('/[a-z]{4,2}/gi');
+			}).equals('<span class="javascript-regexp">/[a-z]{4,2}/gi</span>');
 		});
 	});
 });
